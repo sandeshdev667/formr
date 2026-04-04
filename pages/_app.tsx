4acd1c3066280dad8cd09d34ea501b964fbae4d1
Sandesh Aryal <sxa7784@mavs.uta.edu>
2026-04-04 13:11:33 -0500

    feat:dashboard bug fixed and added web analytics to the vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
+import { Analytics } from '@vercel/analytics/react'
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -11,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div style={{ backgroundColor: '#0D0D0D', minHeight: '100vh' }}>
       <Component {...pageProps} />
+      <Analytics />
     </div>
   )
 }
